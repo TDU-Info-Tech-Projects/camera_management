66c1e558366bdd512dd5fd048d199e8e222e4931
k01234 <105026125+k01234@users.noreply.github.com>
2022-06-03 19:07:47 +0900

    Update db.sql

--- backend/app/db.sql
+++ backend/app/db.sql
@@ -1,11 +1,21 @@
 CREATE EXTENSION pgcrypto;
 CREATE TABLE user (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email_address TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    email_address TEXT NOT NULL UNIQUE,
-    created_on DATE NOT NULL,
+    created_on DATE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE category (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE manufacture (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
 );
 
 CREATE TABLE mount (
@@ -16,18 +26,25 @@ CREATE TABLE mount (
 CREATE TABLE item (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
-    category TEXT, NOT NULL,
-    manufacture TEXT NOT NULL,
-    release_date DATE, NOT NULL,
-    consumable BOOLEAN, NOT NULL,
-    mount_id SERIAL REFERENCES mount(id)
+    consumable BOOLEAN NOT NULL,
+    category SERIAL REFERENCES category(id),
+    manufacture SERIAL REFERENCES manufacture(id),
+    mount_id SERIAL REFERENCES mount(id),
+    release_date DATE
 );
 
 CREATE TABLE loan_item (
     id SERIAL PRIMARY KEY,
-    user_id SERIAL NOT NULL REFERENCES user(id),
+    user_uuid UUID NOT NULL REFERENCES user(uuid),
     item_id SERIAL NOT NULL REFERENCES item(id),
     loan_date DATE NOT NULL,
     due_date DATE NOT NULL,
     return_date DATE
 );
+
+CREATE TABLE button (
+    id SERIAL PRIMARY KEY,
+    user_uuid UUID NOT NULL REFERENCES user(uuid),
+    item_id SERIAL NOT NULL REFERENCES item(id),
+    request_date DATE DEFAULT CURRENT_DATE
+)
