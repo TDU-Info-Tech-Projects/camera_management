d45b5ec42c481fdb1455385a01cbfbe3386676cf
falcon78 <falconhawk@protonmail.com>
2022-06-03 11:48:16 +0000

    add test models

--- backend/controllers/auth.py
+++ backend/controllers/auth.py
@@ -5,6 +5,8 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
+from database.models import User
+
 bp = Blueprint('auth', __name__, url_prefix="/auth")
 
 @bp.route("/register", methods=('GET', 'POST'))
