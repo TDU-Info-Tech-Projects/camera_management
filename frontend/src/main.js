0c7ddb46f6aafed9e9c685245df97bec9fc40f3a
yoCkawa0 <18nc047@ms.dendai.ac.jp>
2022-10-21 21:02:48 +0900

    installed vue-router & vuetify template

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,22 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
+import App from '@/App.vue'
 import vuetify from './plugins/vuetify'
+// import About from '@/About.vue'
+
+
+Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/',      component: App },
+  // { path: '/about', component: About }
+]
+
+const router = new VueRouter({
+  routes
+})
+
 
 Vue.config.productionTip = false
 
